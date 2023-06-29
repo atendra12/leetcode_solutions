05186de84d3e9b4e8353e0c1eb8a845c38db807b
Atendra Kumar <33391781+atendra12@users.noreply.github.com>
2023-06-29 09:04:46 +0000

    Sync LeetCode submission - Two Sum (python3)

--- leetcode/problems/two_sum/solution.py
+++ leetcode/problems/two_sum/solution.py
@@ -1,15 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        my_dict = {}
-        for index in range(len(nums)):
-            my_dict[nums[index]] = index
-        for index in range(len(nums)):
-            try:
-                second_index = my_dict[target-nums[index]]
-                if index != second_index:
-                    return index, second_index
-            except:
-                continue
-        
-      
+        result = dict()
+        for index, item in enumerate(nums):
+            rem = target - item
+            if rem in result.keys():
+                return index, result[rem]
+            else:
+                result[item] = index
+
+
+
         
