ab46f58465b1d868e2aa8d04401a9f457e378058
Atendra Kumar <33391781+atendra12@users.noreply.github.com>
2023-07-03 09:39:20 +0000

    Sync LeetCode submission - Merge Intervals (python3)

--- leetcode/problems/merge_intervals/solution.py
+++ leetcode/problems/merge_intervals/solution.py
@@ -1,24 +1,26 @@
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        intervals.sort(key=lambda x: x[0])
-        merged = []
-        for item in intervals:
-            if (not merged or (merged[-1][1] < item[0])):
-                merged.append(item)   
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        length = len(intervals)
+        if length == 1:
+            return intervals
+        ## let's sort the array
+        intervals = sorted(intervals, key=lambda pair: pair[0])
+
+        first_pt = 0
+        second_pt = first_pt + 1
+        result = []
+        while(second_pt < length):
+            if intervals[second_pt][0] < intervals[first_pt][0]:
+                second_pt, first_pt = first_pt , second_pt
+            if intervals[second_pt][0] > intervals[first_pt][1]:
+                result.append(intervals[first_pt])
+                first_pt = second_pt
+                second_pt+=1
             else:
-                merged[-1][1] = max(merged[-1][1],item[1])
-        return merged
-                
-            
-                
-                
-            
-                
-            
-        
-        
-        
+                min_val = min(intervals[first_pt][0], intervals[second_pt][0])
+                max_val = max(intervals[first_pt][1], intervals[second_pt][1])
+                intervals[first_pt] = [min_val,max_val]
+                second_pt+=1
+
+        result.append(intervals[first_pt])
+        return result
